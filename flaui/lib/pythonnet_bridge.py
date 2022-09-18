75020f5d464ea0cb9a86151fee8a3e59297e7f72
Amruth VVKP <amruthvvkp@gmail.com>
2022-09-18 19:52:57 +0100

    Add Poetry for dependency management and PyPI publishing Fixes #34
    * Successful poetry upgrade
    * Refactored folder for easier build and dist through poetry
    * Updated GitHub actions to include Poetry.

--- flaui/lib/pythonnet_bridge.py
+++ flaui/lib/pythonnet_bridge.py
@@ -3,10 +3,10 @@ import clr
 from pathlib import Path
 from loguru import logger
 
-BIN_HOME = os.path.join(Path(__file__).parent.parent.resolve(), 'bin')
+BIN_HOME = os.path.join(Path(__file__).parent.parent.resolve(), "bin")
 
 
-def setup_pythonnet_bridge():
+def setup_pythonnet_bridge() -> None:
     """Sets up PythonNet bridge
 
     Sets up PythonNet bridge for FlaUI and automation dependencies for UI Automation so that the interlinked C# .NET dependencies are injected into the Python environment listed under flaui/bin folder.
@@ -15,10 +15,10 @@ def setup_pythonnet_bridge():
     """
     try:
         for _ in os.listdir(BIN_HOME):
-            path, dll = os.path.join(BIN_HOME, _), _.replace('.dll', '')
+            path, dll = os.path.join(BIN_HOME, _), _.replace(".dll", "")
             clr.AddReference(path)
             clr.AddReference(dll)
-            logger.info(f'Added {dll} DLL from {path} to PythonNet bridge')
+            logger.info(f"Added {dll} DLL from {path} to PythonNet bridge")
     except Exception as err:
-        logger.exception(f'{err}')
+        logger.exception(f"{err}")
         raise err
--- tests/pytest/unit_tests/flaui/lib/test_pythonnet_bridge.py
+++ tests/pytest/unit_tests/flaui/lib/test_pythonnet_bridge.py
@@ -2,10 +2,11 @@ import pytest
 from loguru import logger
 
 
-def test_setup_pythonnet_bridge():
+def test_setup_pythonnet_bridge() -> None:
     try:
         from flaui.lib.pythonnet_bridge import setup_pythonnet_bridge
+
         setup_pythonnet_bridge()
     except Exception as err:
-        logger.exception(f'{err}')
-        pytest.fail('Failed to setup PythonNet bridge for FlaUI dependencies')
+        logger.exception(f"{err}")
+        pytest.fail("Failed to setup PythonNet bridge for FlaUI dependencies")
