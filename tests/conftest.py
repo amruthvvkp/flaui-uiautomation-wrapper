80bfcea878be386968ae848fc4c4c5f8215ce326
Amruth VVKP <amruthvvkp@gmail.com>
2025-07-30 11:18:30 +0100

    Refactor test fixtures and remove deprecated platform detection for Windows 11

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,19 @@ setup_pythonnet_bridge()
 from loguru import logger
 from _pytest.logging import LogCaptureFixture
 
+import gc
+import time
+
+from flaui.lib.enums import UIAutomationTypes
+from flaui.lib.exceptions import ElementNotFound
+
+from tests.test_utilities.base import FlaUITestBase
+from tests.test_utilities.elements.winforms_application.base import (
+    WinFormsApplicationElements,
+    get_winforms_application_elements,
+)
+from tests.test_utilities.elements.wpf_application.base import WPFApplicationElements, get_wpf_application_elements
+
 
 @pytest.fixture
 def caplog(caplog: LogCaptureFixture) -> Generator[LogCaptureFixture, Any, None]:
@@ -30,3 +43,103 @@ def caplog(caplog: LogCaptureFixture) -> Generator[LogCaptureFixture, Any, None]
     )
     yield caplog
     logger.remove(handler_id)
+
+
+@pytest.fixture(scope="session")
+def launch_all_test_applications() -> Generator[dict[UIAutomationTypes, dict[str, FlaUITestBase]], Any, None]:
+    """Fixture to launch all test applications for the UIAutomation tests."""
+    result = {UIAutomationTypes.UIA2: {}, UIAutomationTypes.UIA3: {}}
+
+    for app_type in ["WinForms", "WPF"]:
+        for ui_automation_type in [UIAutomationTypes.UIA2, UIAutomationTypes.UIA3]:
+            test_base = FlaUITestBase(ui_automation_type, app_type)
+            try:
+                logger.debug(f"Launching test application: {app_type} with {ui_automation_type}")
+                test_base.launch_test_app()
+                time.sleep(1)  # Give time for UI to initialize
+                assert test_base.automation.application, "Application did not start correctly!"
+            except Exception as e:
+                logger.error(f"Error launching test app: {e}")
+                pytest.exit("Test application failed to launch!")
+            else:
+                result[ui_automation_type][app_type] = test_base
+    yield result
+    for ui_automation_type in [UIAutomationTypes.UIA2, UIAutomationTypes.UIA3]:
+        for app_type in ["WinForms", "WPF"]:
+            test_base = result[ui_automation_type][app_type]
+            test_base.close_test_app()
+            gc.collect()
+
+
+@pytest.fixture(
+    scope="session",
+    params=[
+        pytest.param((UIAutomationTypes.UIA2, "WinForms"), id="UIA2_WinForms"),
+        pytest.param((UIAutomationTypes.UIA2, "WPF"), id="UIA2_WPF"),
+        pytest.param((UIAutomationTypes.UIA3, "WinForms"), id="UIA3_WinForms"),
+        pytest.param((UIAutomationTypes.UIA3, "WPF"), id="UIA3_WPF"),
+    ],
+)
+def setup_ui_testing_environment(
+    request: pytest.FixtureRequest, launch_all_test_applications: dict[UIAutomationTypes, dict[str, FlaUITestBase]]
+) -> Generator[FlaUITestBase, Any, None]:
+    """Fixture to launch the test application for the UIAutomation tests."""
+    ui_automation_type, test_application_type = request.param
+
+    yield launch_all_test_applications[ui_automation_type][test_application_type]
+
+
+@pytest.fixture(scope="class", name="test_application")
+def get_ui_test_application(
+    setup_ui_testing_environment: FlaUITestBase,
+) -> Generator[WinFormsApplicationElements | WPFApplicationElements, Any, None]:
+    """Fixture to get the test application for the UIAutomation tests.
+
+    :param setup_ui_testing_environment: Application object.
+    :yield: Application object.
+    """
+    application = setup_ui_testing_environment.automation.application
+    automation = setup_ui_testing_environment.automation.cs_automation
+    test_application_type = setup_ui_testing_environment.app_type
+    elements = (
+        get_winforms_application_elements(application.get_main_window(automation))
+        if test_application_type == "WinForms"
+        else get_wpf_application_elements(application.get_main_window(automation))
+    )
+    try:
+        elements.main_window.find_first_descendant(
+            condition=elements._cf.by_name("Simple Controls")
+        ).as_tab_item().click()
+    except ElementNotFound as e:
+        print(f"Error: {e}")
+    yield elements
+
+
+@pytest.fixture()
+def ui_automation_type(setup_ui_testing_environment: FlaUITestBase) -> Generator[UIAutomationTypes, None, None]:
+    """Fixture to get the UIAutomation type for the UIAutomation tests.
+
+    :param setup_ui_testing_environment: Application object.
+    :yield: UIAutomation type.
+    """
+    yield setup_ui_testing_environment.ui_automation_type
+
+
+@pytest.fixture()
+def test_application_type(setup_ui_testing_environment: FlaUITestBase) -> Generator[str, None, None]:
+    """Fixture to get the test application type for the UIAutomation tests.
+
+    :param setup_ui_testing_environment: Application object.
+    :yield: Test application type.
+    """
+    yield setup_ui_testing_environment.app_type
+
+
+@pytest.fixture()
+def condition_factory(test_application: WinFormsApplicationElements | WPFApplicationElements) -> Generator:
+    """Fixture to get the condition factory for the UIAutomation tests.
+
+    :param test_application: Application object.
+    :yield: Condition factory.
+    """
+    yield test_application._cf
--- tests/ui/core/automation_elements/test_checkbox.py
+++ tests/ui/core/automation_elements/test_checkbox.py
@@ -7,8 +7,8 @@ from flaui.core.automation_elements import CheckBox
 from flaui.core.definitions import ToggleState
 import pytest
 
+from tests.test_utilities.elements.winforms_application.base import WinFormsApplicationElements
 from tests.test_utilities.elements.wpf_application.base import WPFApplicationElements
-from tests.ui.conftest import WinFormsApplicationElements
 
 
 class TestCheckBoxElements:
